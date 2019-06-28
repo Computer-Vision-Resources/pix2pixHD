b1d2893bcf31df8f92c2ba84f18cc03a0c3f2ad3
haotianz <haotianz@stanford.edu>
2019-06-28 14:51:38 -0700

    add table tennis scripts

---
+++ scripts/test_tabletennis_640p_g1.sh
@@ -0,0 +1,2 @@
+### Using labels only
+python test.py --name pose2human_640p_HW --dataroot /Projects/esper_haotian/esper/app/data/image/JZ --label_nc 0 --no_instance --loadSize 640 --batchSize 1 --how_many 100000
---
+++ scripts/test_tabletennis_stick_640p_g1.sh
@@ -0,0 +1,2 @@
+python test.py --name stick2human_640p_g4_HW --dataroot /Projects/esper_haotian/esper/app/result/pix2pixHD/stick2human --label_nc 0 --no_instance --loadSize 640 --batchSize 1 --how_many 100000
+mv results/stick2human_640p_g4_HW/test_latest/images/* /Projects/esper_haotian/esper/app/result/pix2pixHD/stick2human/test_B
