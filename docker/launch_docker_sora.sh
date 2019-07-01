5debca87bb8a1d4d13c874fc68d2eaad75475fa4
haotianz <haotianz@stanford.edu>
2019-07-01 14:00:04 -0700

    fix mount path

--- docker/launch_docker_sora.sh
+++ docker/launch_docker_sora.sh
@@ -1,6 +1,9 @@
 # Thanks @dustinfreeman for providing the script
+# Note:
+# 1. pwd must exists
+# 2. workdir is just an entry point  
 #!/bin/bash
 
 # nvidia-docker build -f docker/Dockerfile -t haotianz/pix2pixHD:CUDA9-py35 .
 
-nvidia-docker run -ti --ipc=host --shm-size 11G -v ~/Projects/pix2pixHD --workdir=/pix2pixHD haotianz/vid2vid:CUDA9-py35 /bin/bash
+nvidia-docker run -ti --ipc=host --shm-size 11G -v $(pwd):/home/haotian/Projects/pix2pixHD --workdir=/home/haotian/Projects/pix2pixHD haotianz/vid2vid:CUDA9-py35 /bin/bash
